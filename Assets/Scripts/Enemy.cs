270ff5144f2182b91d8cec045622dcfcd5206769
Jahvier <jahvierhernandez11@gmail.com>
2020-02-24 21:27:16 -0800

    Update

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    private int maxIndex;
+    private Waypoints Wpoints;
+    public GameObject projectile;
+    public float projectileDamage;
+    public int index;
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+    public int health;
+    public int maxHealth;
+
+    void Start()
+    {
+        Wpoints = GameObject.FindGameObjectWithTag("Waypoints").GetComponent<Waypoints>();
+        maxIndex = Wpoints.waypoints.Length;
+        timeBtwShots = startTimeBtwShots;
+    }
+
+    void Update()
+    {
+        if(index != maxIndex)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Wpoints.waypoints[index].position, speed * Time.deltaTime); // y-axis problem, it would float a bit here
+
+            Vector3 dir = Wpoints.waypoints[index].position - transform.position;
+            float angle = Mathf.Atan2(dir.z, dir.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.down); // The rotation needs to change, try using += for smoother turns
+
+            if(Vector3.Distance(transform.position, Wpoints.waypoints[index].position) < 0.1f)
+                index++;
+
+        }
+        else
+        {
+
+            if(timeBtwShots <= 0 )
+            {
+                Instantiate(projectile, transform.position, Quaternion.identity);
+                timeBtwShots = startTimeBtwShots;
+            }
+            else
+                timeBtwShots -= Time.deltaTime;
+        }
+        //Enemy Fires Projectile goes here
+
+    }
+}
---
+++ Assets/Scripts/MainTower.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainTower : MonoBehaviour
+{
+    public GameObject MT;
+    public int health;
+    public int maxHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
